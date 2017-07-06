e08484f65843b40adef8a578ee36460e75d36c00
James Cox <j-cox03@hotmail.com>
2017-07-06 20:03:58 +0100

    DateManager: changed the way isDateValid works and changed test to represent this

--- app/src/test/java/com/example/james/todolist/DateManagerTest.java
+++ app/src/test/java/com/example/james/todolist/DateManagerTest.java
@@ -15,58 +15,19 @@ public class DateManagerTest {
 
     @Test
     public void canValidateInputDate__DayWrong(){
-        boolean pass = false;
-        try{
-            DateManager.isDateValid(0, 6, 2017);
-        }catch(IllegalArgumentException ex){
-            pass = true;
-        }
-        assertEquals(true, pass);
-
-        pass = false;
-        try{
-            DateManager.isDateValid(32, 6, 2017);
-        }catch(IllegalArgumentException ex){
-            pass = true;
-        }
-        assertEquals(true, pass);
+        assertEquals(false, DateManager.isDateValid(0, 6, 2017));
+        assertEquals(false, DateManager.isDateValid(32, 6, 2017));
     }
 
     @Test
     public void canValidateInputDate__MonthWrong(){
-        boolean pass = false;
-        try{
-            DateManager.isDateValid(1, 0, 2017);
-        }catch(IllegalArgumentException ex){
-            pass = true;
-        }
-        assertEquals(true, pass);
-
-        pass = false;
-        try{
-            DateManager.isDateValid(1, 13, 2017);
-        }catch(IllegalArgumentException ex){
-            pass = true;
-        }
-        assertEquals(true, pass);
+        assertEquals(false, DateManager.isDateValid(1, 0, 2017));
+        assertEquals(false, DateManager.isDateValid(1, 13, 2017));
     }
 
     @Test
     public void canValidateInputDate__YearWrong(){
-        boolean pass = false;
-        try{
-            DateManager.isDateValid(1, 6, 999);
-        }catch(IllegalArgumentException ex){
-            pass = true;
-        }
-        assertEquals(true, pass);
-
-        pass = false;
-        try{
-            DateManager.isDateValid(1, 6, 10_000);
-        }catch(IllegalArgumentException ex){
-            pass = true;
-        }
-        assertEquals(true, pass);
+        assertEquals(false, DateManager.isDateValid(1, 6, 999));
+        assertEquals(false, DateManager.isDateValid(1, 6, 10_000));
     }
 }
