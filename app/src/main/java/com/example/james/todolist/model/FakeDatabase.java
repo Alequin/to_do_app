11e53ad504896ad725a8c26f8bc2a965d091d7fd
James Cox <j-cox03@hotmail.com>
2017-07-06 22:31:23 +0100

    FakeDatabase: Made get size and remove methods. Made all tasks call their save method when build method is called

--- app/src/main/java/com/example/james/todolist/model/FakeDatabase.java
+++ app/src/main/java/com/example/james/todolist/model/FakeDatabase.java
@@ -19,38 +19,46 @@ public class FakeDatabase {
         tasks.add(task);
     }
 
+    public static void remove_by_index(int index){
+        tasks.remove(index);
+    }
+
+    public static int size(){
+        return tasks.size();
+    }
+
     public static void buildDatabase(){
         Calendar date = Calendar.getInstance();
 
         Task task1 = new Task("buy food", "go to the shop", date, date, false);
         task1.setCreationDate(1, 1, 2017);
         task1.setDueDate(2, 1, 2017);
-        tasks.add(task1);
+        task1.save();
 
         Task task2 = new Task("walk dog", "take the dog out", date, date, false);
         task2.setCreationDate(15, 2, 2017);
         task2.setDueDate(30, 2, 2017);
-        tasks.add(task2);
+        task2.save();
 
         Task task3 = new Task("complete work", "", date, date, false);
         task3.setCreationDate(25, 2, 2017);
         task3.setDueDate(10, 3, 2017);
-        tasks.add(task3);
+        task3.save();
 
         Task task4 = new Task("cook food", "find an interesting recipe", date, date, false);
         task4.setCreationDate(4, 3, 2017);
         task4.setDueDate(20, 3, 2017);
-        tasks.add(task4);
+        task4.save();
 
         Task task5 = new Task("iron clothes", "", date, date, false);
         task5.setCreationDate(13, 4, 2017);
         task5.setDueDate(9, 6, 2017);
-        tasks.add(task5);
+        task5.save();
 
         Task task6 = new Task("go for a run", "", date, date, false);
         task6.setCreationDate(17, 5, 2017);
         task6.setDueDate(20, 6, 2017);
-        tasks.add(task6);
+        task6.save();
     }
 
 }
