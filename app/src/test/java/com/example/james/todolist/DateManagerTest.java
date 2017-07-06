53529c2321282e9b02ca87edbd6ce44ec6872ab5
James Cox <j-cox03@hotmail.com>
2017-07-06 20:01:17 +0100

    DateManager: Made tests for validating an input date

--- app/src/test/java/com/example/james/todolist/DateManagerTest.java
+++ app/src/test/java/com/example/james/todolist/DateManagerTest.java
@@ -1,5 +1,7 @@
 package com.example.james.todolist;
 
+import com.example.james.todolist.helper.DateManager;
+
 import org.junit.Test;
 import org.junit.Before;
 
@@ -10,4 +12,61 @@ import static org.junit.Assert.*;
  */
 
 public class DateManagerTest {
+
+    @Test
+    public void canValidateInputDate__DayWrong(){
+        boolean pass = false;
+        try{
+            DateManager.isDateValid(0, 6, 2017);
+        }catch(IllegalArgumentException ex){
+            pass = true;
+        }
+        assertEquals(true, pass);
+
+        pass = false;
+        try{
+            DateManager.isDateValid(32, 6, 2017);
+        }catch(IllegalArgumentException ex){
+            pass = true;
+        }
+        assertEquals(true, pass);
+    }
+
+    @Test
+    public void canValidateInputDate__MonthWrong(){
+        boolean pass = false;
+        try{
+            DateManager.isDateValid(1, 0, 2017);
+        }catch(IllegalArgumentException ex){
+            pass = true;
+        }
+        assertEquals(true, pass);
+
+        pass = false;
+        try{
+            DateManager.isDateValid(1, 13, 2017);
+        }catch(IllegalArgumentException ex){
+            pass = true;
+        }
+        assertEquals(true, pass);
+    }
+
+    @Test
+    public void canValidateInputDate__YearWrong(){
+        boolean pass = false;
+        try{
+            DateManager.isDateValid(1, 6, 999);
+        }catch(IllegalArgumentException ex){
+            pass = true;
+        }
+        assertEquals(true, pass);
+
+        pass = false;
+        try{
+            DateManager.isDateValid(1, 6, 10_000);
+        }catch(IllegalArgumentException ex){
+            pass = true;
+        }
+        assertEquals(true, pass);
+    }
 }
