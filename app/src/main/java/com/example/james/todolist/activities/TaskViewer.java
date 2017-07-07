87495eb2b4d7be855f193d23529007edc3b850bf
James Cox <j-cox03@hotmail.com>
2017-07-07 10:32:25 +0100

    Made empty files for TaskViewerActivity

---
+++ app/src/main/java/com/example/james/todolist/activities/TaskViewer.java
@@ -0,0 +1,15 @@
+package com.example.james.todolist.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.james.todolist.R;
+
+public class TaskViewer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_viewer);
+    }
+}
