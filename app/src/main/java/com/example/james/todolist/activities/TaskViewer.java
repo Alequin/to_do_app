5de735619c14dff86e0830e1e34004a74c5f5557
James Cox <j-cox03@hotmail.com>
2017-07-07 19:52:22 +0100

    TaskViewer: Added basic setup code to onCreate

--- app/src/main/java/com/example/james/todolist/activities/TaskViewer.java
+++ app/src/main/java/com/example/james/todolist/activities/TaskViewer.java
@@ -2,8 +2,16 @@ package com.example.james.todolist.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import com.example.james.todolist.R;
+import com.example.james.todolist.model.Task;
+import com.google.gson.Gson;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class TaskViewer extends AppCompatActivity {
 
@@ -11,5 +19,17 @@ public class TaskViewer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_viewer);
+
+        Task task = (Task) getIntent().getSerializableExtra(TaskList.TASK_EXTRA);
+
+        CheckBox checkBox = (CheckBox) findViewById(R.id.check_box_task_viewer_activity);
+        TextView outline = (TextView) findViewById(R.id.outline_text_view_task_viewer_activity);
+        TextView creationDate = (TextView) findViewById(R.id.creation_date_info_view_task_viewer_activity);
+        TextView dueDate = (TextView) findViewById(R.id.due_date_info_view_task_viewer_activity);
+
+        checkBox.setChecked(task.isComplete());
+        outline.setText(task.getOutline());
+        creationDate.setText(task.getFormattedCreationDate());
+        dueDate.setText(task.getFormattedDueDate());
     }
 }
