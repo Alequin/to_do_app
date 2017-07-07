117652e2308a7841ba1868dda560fd6d7d6e2d4a
James Cox <j-cox03@hotmail.com>
2017-07-07 20:52:16 +0100

    Added code for update button on Task Viewer. Currently Update method does not work

--- app/src/main/java/com/example/james/todolist/activities/TaskViewer.java
+++ app/src/main/java/com/example/james/todolist/activities/TaskViewer.java
@@ -2,7 +2,9 @@ package com.example.james.todolist.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.james.todolist.R;
@@ -15,25 +17,36 @@ import java.util.ArrayList;
 
 public class TaskViewer extends AppCompatActivity {
 
+    private CheckBox checkBox;
+    private EditText extraDetails;
+
+    private Task currentTask;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_viewer);
 
-        Task task = (Task) getIntent().getSerializableExtra(TaskList.TASK_EXTRA);
+        currentTask = (Task) getIntent().getSerializableExtra(TaskList.TASK_EXTRA);
 
-        CheckBox checkBox = (CheckBox) findViewById(R.id.check_box_task_viewer_activity);
+        checkBox = (CheckBox) findViewById(R.id.check_box_task_viewer_activity);
         TextView outline = (TextView) findViewById(R.id.outline_text_view_task_viewer_activity);
         TextView creationDate = (TextView) findViewById(R.id.creation_date_info_view_task_viewer_activity);
         TextView dueDate = (TextView) findViewById(R.id.due_date_info_view_task_viewer_activity);
+        extraDetails = (EditText) findViewById(R.id.extra_details_view_task_viewer_activity);
 
-        checkBox.setChecked(task.isComplete());
-        outline.setText(task.getOutline());
+        checkBox.setChecked(currentTask.isComplete());
+        outline.setText(currentTask.getOutline());
 
         String dateLayout = "%s: %s";
-        String creationDateText = String.format(dateLayout, getString(R.string.creation_date), task.getFormattedCreationDate());
+        String creationDateText = String.format(dateLayout, getString(R.string.creation_date), currentTask.getFormattedCreationDate());
         creationDate.setText(creationDateText);
-        String dueDateText = String.format(dateLayout, getString(R.string.due_date), task.getFormattedDueDate());
+        String dueDateText = String.format(dateLayout, getString(R.string.due_date), currentTask.getFormattedDueDate());
         dueDate.setText(dueDateText);
+    }
+
+    public void onClickUpdateButton(View view){
+        currentTask.setExtraDetails(extraDetails.getText().toString());
+        currentTask.update();
     }
 }
