024c50eaf78b1574a99cc4f71eee5c2ae70f567f
James Cox <j-cox03@hotmail.com>
2017-07-07 11:43:25 +0100

    Made custom application

---
+++ app/src/main/java/com/example/james/todolist/application/TaskListApplication.java
@@ -0,0 +1,15 @@
+package com.example.james.todolist.application;
+
+import android.app.Application;
+
+/**
+ * Created by james on 07/07/2017.
+ */
+
+public class TaskListApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
