45b9449b32ee00035993b1ce5f5a9d8025f7c9ad
James Cox <j-cox03@hotmail.com>
2017-07-07 20:13:15 +0100

    Made second Calendar object in FakeDatabase.buildDatabase so when seDueDate is called it does not override changes from setCreationDate

--- app/src/main/java/com/example/james/todolist/model/FakeDatabase.java
+++ app/src/main/java/com/example/james/todolist/model/FakeDatabase.java
@@ -36,33 +36,34 @@ public class FakeDatabase {
 
     public static void buildDatabase(){
         Calendar date = Calendar.getInstance();
+        Calendar date2 = Calendar.getInstance();
 
-        Task task1 = new Task("buy food", "go to the shop", date, date, true);
+        Task task1 = new Task("buy food", "go to the shop", date, date2, true);
         task1.setCreationDate(1, 1, 2017);
         task1.setDueDate(2, 1, 2017);
         task1.save();
 
-        Task task2 = new Task("walk dog", "take the dog out", date, date, false);
+        Task task2 = new Task("walk dog", "take the dog out", date, date2, false);
         task2.setCreationDate(15, 2, 2017);
         task2.setDueDate(30, 2, 2017);
         task2.save();
 
-        Task task3 = new Task("complete work", "", date, date, false);
+        Task task3 = new Task("complete work", "", date, date2, false);
         task3.setCreationDate(25, 2, 2017);
         task3.setDueDate(10, 3, 2017);
         task3.save();
 
-        Task task4 = new Task("cook food", "find an interesting recipe", date, date, false);
+        Task task4 = new Task("cook food", "find an interesting recipe", date, date2, false);
         task4.setCreationDate(4, 3, 2017);
         task4.setDueDate(20, 3, 2017);
         task4.save();
 
-        Task task5 = new Task("iron clothes", "", date, date, false);
+        Task task5 = new Task("iron clothes", "", date, date2, false);
         task5.setCreationDate(13, 4, 2017);
         task5.setDueDate(9, 6, 2017);
         task5.save();
 
-        Task task6 = new Task("go for a run", "", date, date, false);
+        Task task6 = new Task("go for a run", "", date, date2, false);
         task6.setCreationDate(17, 5, 2017);
         task6.setDueDate(20, 6, 2017);
         task6.save();
