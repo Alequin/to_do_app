41e036e43f78b1a70d73b114606536ad327d27c7
James Cox <j-cox03@hotmail.com>
2017-07-08 09:49:16 +0100

    Due date button in NewTaskActivity now takes user to date picker page

--- app/src/main/java/com/example/james/todolist/activities/NewTaskActivity.java
+++ app/src/main/java/com/example/james/todolist/activities/NewTaskActivity.java
@@ -1,7 +1,9 @@
 package com.example.james.todolist.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.james.todolist.R;
 
@@ -11,5 +13,9 @@ public class NewTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
+    }
+
+    public void onClickDueDateButton(View view){
+        startActivity(new Intent(this, DateActivity.class));
     }
 }
