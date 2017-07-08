9149b6f6343d6b085dd8576eeab54b46649256d7
James Cox <j-cox03@hotmail.com>
2017-07-08 04:14:21 +0100

    Added id to TaskViewer bin icon and made code to run on bin icon click (task delete method not created)

--- app/src/main/java/com/example/james/todolist/activities/TaskViewer.java
+++ app/src/main/java/com/example/james/todolist/activities/TaskViewer.java
@@ -4,10 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.james.todolist.R;
 import com.example.james.todolist.model.Task;
@@ -48,6 +50,15 @@ public class TaskViewer extends AppCompatActivity {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.task_viewer_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId() == R.id.bin_icon_task_viewer_activity){
+            currentTask.delete();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void prepareDateViews(){
