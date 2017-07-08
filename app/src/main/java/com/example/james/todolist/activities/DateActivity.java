44730c62a6b238f2295cdb00a8dd225c1e0bf5e6
James Cox <j-cox03@hotmail.com>
2017-07-08 10:10:52 +0100

    DateActivity: Select date button warns users if selected date is before the current date. Does nothing for valid dates yet

--- app/src/main/java/com/example/james/todolist/activities/DateActivity.java
+++ app/src/main/java/com/example/james/todolist/activities/DateActivity.java
@@ -3,16 +3,39 @@ package com.example.james.todolist.activities;
 import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 import com.example.james.todolist.R;
 
+import java.util.Calendar;
+
 public class DateActivity extends AppCompatActivity {
+
+    private DatePicker datePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_date);
         hideActionBar();
+
+        datePicker = (DatePicker) findViewById(R.id.date_picker_date_activity);
+    }
+
+    public void onClickSelectDate(View view){
+
+        Calendar today = Calendar.getInstance();
+        Calendar selected = getCalendarFromDatePicker();
+
+        if(selected.before(today)){
+            Toast toast = Toast.makeText(this, getString(R.string.invalid_date_message), Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }else{
+
+        }
     }
 
     private void hideActionBar(){
@@ -22,5 +45,16 @@ public class DateActivity extends AppCompatActivity {
         }else{
             getSupportActionBar().hide();
         }
+    }
+
+    private Calendar getCalendarFromDatePicker(){
+
+        int day = datePicker.getDayOfMonth();
+        int month = datePicker.getMonth();
+        int year = datePicker.getYear();
+
+        Calendar input = Calendar.getInstance();
+        input.set(year, month, day);
+        return input;
     }
 }
