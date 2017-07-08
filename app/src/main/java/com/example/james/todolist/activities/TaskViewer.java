5d6867c1131a9f9fdbaebe7a55d879ab210322f2
James Cox <j-cox03@hotmail.com>
2017-07-08 04:10:53 +0100

    Made top bar menu in TaskViewer and added icons to drawable file

--- app/src/main/java/com/example/james/todolist/activities/TaskViewer.java
+++ app/src/main/java/com/example/james/todolist/activities/TaskViewer.java
@@ -2,6 +2,8 @@ package com.example.james.todolist.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -39,6 +41,13 @@ public class TaskViewer extends AppCompatActivity {
 
         extraDetails = (EditText) findViewById(R.id.extra_details_view_task_viewer_activity);
         extraDetails.setText(currentTask.getExtraDetails());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.task_viewer_menu, menu);
+        return true;
     }
 
     private void prepareDateViews(){
